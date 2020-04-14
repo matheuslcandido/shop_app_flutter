931ea778bed415be8b5f3ff214e20bdcff6b7190
matheuslcandido <matheus.l.candido@gmail.com>
2020-04-14 00:19:09 -0300

    adding the provider of products

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -16,9 +16,17 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              '/product-detail',
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
