9694bba64a1e62a5f88bb60d1780a01c54b485d3
matheuslcandido <matheus.l.candido@gmail.com>
2020-04-18 01:05:26 -0300

    adding the logic to remove products from the cart

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -25,6 +25,16 @@ class Cart with ChangeNotifier {
     return _items.length;
   }
 
+  double get totalAmount {
+    var total = 0.0;
+
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    }); 
+
+    return total;
+  }
+
   void addItem(String productId, double price, String title) {
     if (_items.containsKey(productId)) {
       _items.update(
@@ -48,4 +58,9 @@ class Cart with ChangeNotifier {
     }
     notifyListeners();
   }
+
+   void removeItem(String productId) {
+     _items.remove(productId);
+     notifyListeners();
+   }
 }
