c5971499f703693b237a917890c1d44c248dce2f
matheuslcandido <matheus.l.candido@gmail.com>
2020-04-19 22:35:59 -0300

    adding the logic to add new products to the show

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -49,4 +49,17 @@ class Products with ChangeNotifier {
   Product findById(String id) {
     return _items.firstWhere((product) => product.id == id);
   }
+
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
+
+    notifyListeners();
+  }
 }
